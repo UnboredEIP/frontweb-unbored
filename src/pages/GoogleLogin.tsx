2ce4c1821fca3b851894d19012e6458e2b2430ff
steevenakintilo <steeven-charles-edward.akintilo@epitech.eu>
2024-04-02 21:47:30 +0200

    [ADD] Fully working oauth2 google

--- src/pages/GoogleLogin.tsx
+++ src/pages/GoogleLogin.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { GoogleOAuthProvider, GoogleLogin, useGoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from "jwt-decode";
+import { useToast } from "@chakra-ui/react";
+import { Navigate, useNavigate } from "react-router-dom";
 
 // Define the prop types for MyCustomButton
 interface MyCustomButtonProps {
@@ -14,9 +16,8 @@ const MyCustomButton: React.FC<MyCustomButtonProps> = ({ onClick, children }) =>
 );
 
 
-async function makeLoginRequest(
-  email: string,
-) {
+async function makeLoginRequest(email: string, navigate: ReturnType<typeof useNavigate>,showToast: (text: string) => void) {
+  
   try {
     const response = await fetch("http://20.216.143.86/auth/login", {
       method: "POST",
@@ -29,21 +30,22 @@ async function makeLoginRequest(
       const data = await response.json();
       console.log(data);
       localStorage.setItem("token", data["token"]);
+      navigate("/home");
+      window.location.reload();
       return true;
     } else {
       console.error("Login error");
+      showToast("Login with google error");
       return false;
     }
   } catch (error) {
     console.error("Request error: ", error);
+    showToast("Login with google error");
     return false;
   }
 }
 
-async function makeRegisterRequest(
-  email: string,
-  username: string,
-) {
+async function makeRegisterRequest(email: string, username: string, navigate: ReturnType<typeof useNavigate>, showToast: (text: string) => void) {
   try {
     const response = await fetch("http://20.216.143.86/auth/register", {
       method: "POST",
@@ -65,16 +67,32 @@ async function makeRegisterRequest(
     } else {
       const data = await response.json();
       console.error("Register Error ca:", data.error);
+      showToast("Account already exist");
       //console.error("Register Error");
       return false;
     }
   } catch (error) {
     console.error("Request error: ", error);
+    showToast("Register with Ouath2 error");
     return false;
   }
 }
 
 const GoogleOAuthLogin = () => {
+  const navigate = useNavigate(); // Move this line inside your component
+
+  const toast = useToast();
+
+  const showToast = (text: string) => {
+    toast({
+      title: "Ouath2 google",
+      description: text,
+      duration: 5000,
+      isClosable: true,
+      colorScheme: "red",
+    });
+  };
+
   const login = useGoogleLogin({
     onSuccess: async (tokenResponse) => {
       //console.log("email reponse: " , tokenResponse);
@@ -87,6 +105,7 @@ const GoogleOAuthLogin = () => {
     }
   });
 
+
   return (
     <div>
       <div>
@@ -94,15 +113,16 @@ const GoogleOAuthLogin = () => {
           onSuccess={(credentialResponse) => {
             //console.log("toto gogo coco " , credentialResponse.credential);
             const decoded = jwtDecode(JSON.stringify(credentialResponse.credential));
-            console.log("Repkkpkp finale " , decoded.email,decoded.name,"dododo");
+            //console.log("Repkkpkp finale " , decoded.email,decoded.name,"dododo");
             const isLoginPage = window.location.pathname.includes('login');
-            console.log("test " , isLoginPage);
-            if (isLoginPage == false) { 
-              makeRegisterRequest(decoded.email,decoded.name);
+            //console.log("test " , isLoginPage);
+            if (isLoginPage === false) { 
+              makeRegisterRequest(decoded.email,decoded.name,navigate,showToast);
             }
 
             else {
-              makeLoginRequest(decoded.email);
+              makeLoginRequest(decoded.email,navigate,showToast);
+              console.log("je suis ici");
             }
 
           }}
