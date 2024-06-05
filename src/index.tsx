29ccf4941e2651b624bf158acd707785e13b1c9d
Pzerotun <antonin.pottin@epitech.eu>
2024-06-06 01:19:32 +0200

    [FIX] fixed wrong name for App + the last push added the strong password check + same avatar id for clothes

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import App from './App2';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from "@chakra-ui/react";
 
